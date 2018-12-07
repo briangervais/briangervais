76b08af778c77fb2ec444af758b278a84d9ae964
Brian Gervais <briangervais@Brians-MacBook-Pro-4.local>
2018-12-07 14:54:20 -0700

    Updating about page, global components

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
-  var int = window.setInterval(checkSlides, 200);
+  var int = window.setInterval(checkSlides, 250);
   var slides = $('.slide');
   var articles = $('.section--3up');
 
   function checkSlides() {
     articles.addClass('animate-show');
-    
+
     if (slides.is(':visible')) {
       $('.slide:last-of-type').remove();
     }
